8f31722a2878b3b65f1c1edf5d4b11e4c89000a3
Rob Hudson <robhudson@mozilla.com>
2020-06-12 09:13:52 -0700

    Incorporate GLAM's probe processes

--- probe_search/import.py
+++ probe_search/import.py
@@ -3,6 +3,7 @@ import datetime
 from mozilla_schema_generator.glean_ping import GleanPing
 from mozilla_schema_generator.main_ping import MainPing
 from peewee import EXCLUDED, chunked, fn
+from google.cloud import bigquery
 
 from probe_search.db import Probes, db
 from probe_search.utils import snake_case
@@ -17,16 +18,58 @@ def log(message):
     )
 
 
+def get_processes_per_probe():
+    """
+    Returns a dict keyed by metric with the processes this probe has been
+    recorded in.
+
+    For Firefox Desktop only.
+
+    """
+
+    bq = bigquery.Client()
+
+    query = """
+        SELECT
+            metric,
+            ARRAY_AGG(DISTINCT(process)) AS processes,
+        FROM
+            `moz-fx-data-shared-prod.telemetry_derived.client_probe_counts_v1`
+        GROUP BY
+            metric
+    """
+    query_config = bigquery.QueryJobConfig()
+    job1 = bq.query(query, job_config=query_config)
+    result = job1.result()
+
+    processes = {}
+    if result.total_rows > 0:
+        for row in result:
+            processes[row.metric] = row.processes
+
+    print(
+        f" - Fetched {len(processes.keys()):,} probe processes from "
+        "GLAM's BigQuery table."
+    )
+    return processes
+
+
 def import_probes(product, probes):
     data = []
+    if product == "desktop":
+        processes = get_processes_per_probe()
+
     for probe in probes:
+        name = snake_case(probe.name)
         definition = probe.definition
+        if product == "desktop":
+            definition["seen_in_processes"] = processes.get(name, [])
         description = definition["description"]
 
         data.append(
             {
                 "product": product,
-                "name": snake_case(probe.name),
+                "name": name,
                 "type": probe.type,
                 "description": description,
                 "definition": definition,
